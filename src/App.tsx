19aba065908cfd79115212264c8b1dbc9e599147
gremble0 <hermanstornes@gmail.com>
2023-04-23 16:07:29 +0200

    New fonts and layout for title and socials

--- src/App.tsx
+++ src/App.tsx
@@ -21,9 +21,18 @@ function App() {
 
     return (
         <div className = "app" onLoad = { addSocialEventListeners }>
-            <h1 className = "title">gremble.dev</h1>
+            <div className = "title-wrapper">
+                <h2>Hi, I'm</h2>
+                <h1 className = "title-name">gremble</h1>
+                <p>Software Engineer</p>
+            </div>
+            
             
             <div className = "socials">
+                <h1 className = "title-socials">My Socials</h1>
+                <div className = "linkedin social">
+                    <p className = "social-link">Linkedin</p>
+                </div>
                 <div className = "discord social">
                     <p className = "social-link">Discord gremble:1534</p>
                     <img src = {discordLogo} className = "social-logo" />

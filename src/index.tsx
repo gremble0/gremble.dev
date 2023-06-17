f062b1ef0172b87d6568494bc72170df8984e9c1
gremble0 <hermanstornes@gmail.com>
2023-06-17 13:12:33 +0200

    Started carousel for personal projects

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Carousel from "react-elastic-carousel"
+import Item from "./item"
 
 function openInNewTab(url: string) {
     window.open(url, "_blank", "noreferrer")
@@ -10,8 +12,16 @@ function discordOnClick() {
     navigator.clipboard.writeText("gremble");
 }
 
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
+
 function App() {
     document.title = "gremble.dev";
+    const items = [1, 2, 3, 4];
 
     return (
         <div className = "app">
@@ -25,15 +35,15 @@ function App() {
                 <h1 className = "title-socials">Contact me</h1>
                 <div className = "linkedin social" onClick = { () => openInNewTab("https://www.linkedin.com/in/herman-stornes-537308273") }>
                     <p className = "social-link">Linkedin</p>
-                    <img src = {require("./assets/images/linkedin.png")} className = "social-logo" />
+                    <img src = { require("./assets/images/linkedin.png") } className = "social-logo" />
                 </div>
                 <div className = "discord social" onClick = { () => discordOnClick() }>
                     <p className = "social-link">Discord: gremble <a className = "nerdfont">󱓥</a></p>
-                    <img src = {require("./assets/images/discord2.png")} className = "social-logo" />
+                    <img src = { require("./assets/images/discord2.png") } className = "social-logo" />
                 </div>
                 <div className = "github social" onClick = { () => openInNewTab("https://github.com/gremble0") }>
                     <p className = "social-link">Github</p>
-                    <img src = {require("./assets/images/github.png")} className = "social-logo" />
+                    <img src = { require("./assets/images/github.png") } className = "social-logo" />
                 </div>
             </div>
 

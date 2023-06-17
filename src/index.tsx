af3939cd8d24e4a49f2157c61225108ae4f43f85
gremble0 <hermanstornes@gmail.com>
2023-06-17 12:52:01 +0200

    Removed unnecessary template files

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+function openInNewTab(url: string) {
+    window.open(url, "_blank", "noreferrer")
+}
+
+function discordOnClick() {
+    navigator.clipboard.writeText("gremble");
+}
+
+function App() {
+    document.title = "gremble.dev";
+
+    return (
+        <div className = "app">
+            <div className = "title-wrapper">
+                <h2>Hi, I'm</h2>
+                <h1 className = "title-name">gremble</h1>
+                <p>Software Engineer</p>
+            </div>
+            
+            <div className = "socials">
+                <h1 className = "title-socials">Contact me</h1>
+                <div className = "linkedin social" onClick = { () => openInNewTab("https://www.linkedin.com/in/herman-stornes-537308273") }>
+                    <p className = "social-link">Linkedin</p>
+                    <img src = {require("./assets/images/linkedin.png")} className = "social-logo" />
+                </div>
+                <div className = "discord social" onClick = { () => discordOnClick() }>
+                    <p className = "social-link">Discord: gremble <a className = "nerdfont">󱓥</a></p>
+                    <img src = {require("./assets/images/discord2.png")} className = "social-logo" />
+                </div>
+                <div className = "github social" onClick = { () => openInNewTab("https://github.com/gremble0") }>
+                    <p className = "social-link">Github</p>
+                    <img src = {require("./assets/images/github.png")} className = "social-logo" />
+                </div>
+            </div>
+
+            <div className = "projects">
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

e817aa1236bbfd804acf619a18ac81cb0f37d1b8
gremble0 <hermanstornes@gmail.com>
2024-06-20 06:34:02 +0200

    Add nonstdlib project

--- src/constants.tsx
+++ src/constants.tsx
@@ -18,7 +18,8 @@ const projects: ProjectProps[] = [
                     <li>A news screen displaying recent stargazing events that updates whenever
                         relevant news are broadcasted by some select news sources</li>
                 </ul>
-                <p>For this project i was largely in charge of implementing the home screen and the interactive map, as well as managing large amounts of the <b>documentation</b>. For more details about the project check out the <a href="https://github.com/gremble0/nattklar"> github!</a></p>
+                <p>For this project i was largely in charge of implementing the home screen and the interactive map, as well as managing large amounts of the <b>documentation</b></p>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/nattklar">github page</a></p>
             </div>
         ),
     },
@@ -29,6 +30,28 @@ const projects: ProjectProps[] = [
         description: (
             <div className="project-description">
                 <p>Kebab is a a <b>strongly typed</b>, <b>functional programming language</b>. I developed it from scratch in <b>C</b> using no external libraries except <a href="https://github.com/gremble0/nonstdlib">nonstdlib</a> which I started developing before kebab, but much of this library's development happened in parallel with the programming language as I saw the requirements unfold. See the section for nonstdlib for more information about that project.</p>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/kebab">github page</a></p>
+            </div>
+        ),
+    },
+    {
+        projectName: "nonstdlib",
+        imageSrc: "/assets/images/nonstdlib.png",
+        url: "https://github.com/gremble0/nonstdlib",
+        description: (
+            <div className="project-description">
+                <p>Nonstdlib is a library for <b>C</b> that focuses on implementing some useful datastructures and algorithms that are absent from the C standard library.</p>
+                <p>Here are the modules currently implemented in this library</p>
+                <ul>
+                    <li><b>nhashtable.h:</b> Type and functions for a typical hashtable data structure.</li>
+                    <li><b>nerror.h:</b> Contains some functions for handling typical errors and printing more useful debug information. Also has a custom assert macro that uses nonstdlib's error handling on error</li>
+                    <li><b>nlinked_list.h:</b> Type and functions for the linked list data structure.</li>
+                    <li><b>nlist.h (WIP):</b> Type and functions for the list data structure.</li>
+                    <li><b>narray.h:</b> Utilities for normal C arrays.</li>
+                    <li><b>nstring.h:</b> Simple string interface that saves the length of strings instead of relying on nullbytes.</li>
+                    <li><b>nsort.h:</b> Some sorting algorithms for builtin C arrays.</li>
+                </ul>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/nonstdlib">github page</a></p>
             </div>
         ),
     },
@@ -52,6 +75,7 @@ const projects: ProjectProps[] = [
             <div className="project-description">
                 <p>Cchat is the GUI client for its sister project Gochat - the server application for Cchat.</p>
                 <p>Cchat uses <b>Raylib</b> to display the GUI and the standard <b>C</b> library to maintain separate <b>threads</b> for the different components of the program as well as communicating with the server over a <b>TCP</b> connection</p>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/cchat">github page</a></p>
             </div>
         ),
     },
@@ -63,8 +87,8 @@ const projects: ProjectProps[] = [
             <div className="project-description">
                 <p>Gochat is the server application for its sister project Cchat - the GUI client for Gochat</p>
                 <p>As the creative name implies gochat is written in <b>Go</b></p>
-                <p>Similar to gremblebot gochat also logs information about users and messages that connect
-                    to the server with a <b>postgres database</b>.</p>
+                <p>Similar to gremblebot gochat also logs information about users and messages that connect to the server with a <b>postgres database</b>.</p>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/gochat">github page</a></p>
             </div>
         ),
     },
@@ -75,6 +99,7 @@ const projects: ProjectProps[] = [
         description: (
             <div className="project-description">
                 <p>This project is a GUI implementation of the classic Snake game written in <b>Java</b></p>
+                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/snake">github page</a></p>
             </div>
         ),
     },

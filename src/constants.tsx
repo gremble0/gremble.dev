5d3aa6ca2c95ee5855ab474c4f558292d12f741a
gremble0 <hermanstornes@gmail.com>
2024-08-17 22:49:26 +0200

    Update kebab project description, remove snake

--- src/constants.tsx
+++ src/constants.tsx
@@ -27,7 +27,15 @@ const projects: ProjectProps[] = [
         url: "https://github.com/gremble0/kebab",
         description: (
             <div className="project-description">
-                <p>Kebab is a a <b>strongly typed</b>, <b>functional programming language</b>. It consists of two main parts: a <b>compiler</b> written in <b>C++</b> with the <b>LLVM</b> compiler toolchain, and an interpreter that I developed from scratch in <b>C</b> using no external libraries except <a href="https://github.com/gremble0/nonstdlib">nonstdlib</a> which I started developing before kebab. Much of this library's development happened in parallel with the programming language as I saw the requirements unfold. See the section for nonstdlib for more information about that project. The project also has an extensive <b>test suite</b>. The compiler uses googles <b>GoogleTest</b> test framework, while the interpreter uses some internally developed testing functionality largely from <b>nonstdlib</b>.</p>
+                <p>Kebab is a a <b>strongly typed</b>, <b>functional programming language</b>. It has two implementations, a compiler and an interpreter. Both the interpreter and compiler includes detailed error checking and strong type requirements from the programmer to give clear error messages. The projects development is also supported by <b>SonarCloud's static analysis tools.</b></p>
+                <ul>
+                    <li>
+                        The <b>compiler</b> is written in <b>C++</b> with the <b>LLVM</b> compiler toolchain. The compiler also uses googles <b>GoogleTest</b> testing framework.
+                    </li>
+                    <li>
+                        The <b>interpreter</b> that I developed from scratch in <b>C</b> using no external libraries except <a href="https://github.com/gremble0/nonstdlib">nonstdlib</a> which I started developing before kebab. Much of this library's development happened in parallel with the programming language as I saw the requirements unfold. For testing the interpreter uses some internally developed testing functionality largely from <b>nonstdlib</b>.
+                    </li>
+                </ul>
                 <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/kebab">github page</a></p>
             </div>
         ),
@@ -38,7 +46,7 @@ const projects: ProjectProps[] = [
         url: "https://github.com/gremble0/nonstdlib",
         description: (
             <div className="project-description">
-                <p>Nonstdlib is a library for <b>C</b> that focuses on implementing some useful <b>datastructures and algorithms</b> that are absent from the C standard library. Its development is supported by SonarCloud's <b>static analysis tools</b> to ensure consistent interfaces, detecting possible memory leaks, etc.</p>
+                <p>Nonstdlib is a library for <b>C</b> that focuses on implementing some useful <b>datastructures and algorithms</b> that are absent from the C standard library. Its development is supported by <b>SonarCloud's static analysis tools</b> to ensure consistent interfaces, detecting possible memory leaks, etc.</p>
                 <p>Here are the modules currently implemented in this library</p>
                 <ul>
                     <li><b>nhashtable.h</b>: An implementation of the hashtable datastructure - associate a key with some given value with. O(1) insertion complexity, O(1) lookup complexity.</li>
@@ -88,17 +96,6 @@ const projects: ProjectProps[] = [
                 <p>As the creative name implies gochat is written in <b>Go</b></p>
                 <p>Similar to gremblebot gochat also logs information about users and messages that connect to the server with a <b>postgres database</b>.</p>
                 <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/gochat">github page</a></p>
-            </div>
-        ),
-    },
-    {
-        projectName: "snake",
-        imageSrc: "/assets/images/snake.png",
-        url: "https://github.com/gremble0/snake",
-        description: (
-            <div className="project-description">
-                <p>This project is a GUI implementation of the classic Snake game written in <b>Java</b> using the <b>AWT</b> and <b>Swing</b></p>
-                <p>For a more detailed description see the <b>README.md</b> in the projects <a href="https://github.com/gremble0/snake">github page</a></p>
             </div>
         ),
     },
